7501a915b5c945fc8421f6c55221068fb9de0fa3
Vivashwan <98079009+Vivashwan@users.noreply.github.com>
2023-09-19 21:46:59 +0530

    Add files via upload

---
+++ FUND-IT-main/web3/contracts/Untitled.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countOccurrences(vector<int> &arr, int x)
+{
+    int n = arr.size();
+
+    
+
+}
+
+int main()
+{
+    vector<int> arr = {1, 1, 2, 2, 2, 2, 3};
+    vector<int> arr = {1, 1, 2, 2, 2, 2, 3};
+    int x = 2;
+
+    cout<<countOccurrences(arr, x);
+    return 0;
+}
